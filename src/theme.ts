7c1b5c2f042fc46739658bd3e50d2cafc9696bb7
Bobby Lee <ldg@dgit.co>
2023-01-24 17:16:46 +0900

    init

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  green: "rgb(0, 255, 128)",
+  black: {
+    veryDark: "#141414",
+    darker: "#181818",
+    lighter: "#2F2F2F",
+  },
+  white: {
+    lighter: "#fff",
+    darker: "#e5e5e5",
+  },
+};
