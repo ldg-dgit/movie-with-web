063463d075dbfe99e562f1effd77ba4d3a8a114b
Bobby Lee <ldg@dgit.co>
2023-05-10 11:28:24 +0900

    add style

--- graphql-client/src/routes/Movie.js
+++ graphql-client/src/routes/Movie.js
@@ -1,3 +1,71 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const GET_MOVIE = gql`
+	query getMovie($movieId: String!) {
+		movie(id: $movieId) {
+			id
+			title
+			medium_cover_image
+			rating
+		}
+	}
+`;
+
+const Container = styled.div`
+	height: 100vh;
+	background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+	width: 100%;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	color: white;
+`;
+
+const Column = styled.div`
+	margin-left: 10px;
+	width: 50%;
+`;
+
+const Title = styled.h1`
+	font-size: 65px;
+	margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+	font-size: 35px;
+	margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+	font-size: 28px;
+`;
+
+const Image = styled.div`
+	width: 25%;
+	height: 60%;
+	background-color: transparent;
+	background-image: url(${(props) => props.bg});
+	background-size: cover;
+	background-position: center center;
+	border-radius: 7px;
+`;
+
 export default function Movie() {
-	return <div>This is movie detail</div>;
+	const { id } = useParams();
+	const { data, loading } = useQuery(GET_MOVIE, {
+		variables: {
+			movieId: id,
+		},
+	});
+	return (
+		<Container>
+			<Column>
+				<Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
+				<Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+			</Column>
+			<Image bg={data?.movie?.medium_cover_image} />
+		</Container>
+	);
 }
--- graphql-client/src/routes/Movies.js
+++ graphql-client/src/routes/Movies.js
@@ -1,28 +1,92 @@
-import { gql, useApolloClient } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const ALL_MOVIES = gql`
+	query getMovies {
+		allMovies {
+			title
+			id
+			medium_cover_image
+		}
+	}
+`;
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+`;
+
+const Header = styled.header`
+	background-image: linear-gradient(-45deg, #66cc66, #4444ff);
+	height: 45vh;
+	color: white;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+`;
+
+const Title = styled.h1`
+	font-size: 60px;
+	font-weight: 600;
+	margin-bottom: 20px;
+`;
+
+const Loading = styled.div`
+	font-size: 18px;
+	opacity: 0.5;
+	font-weight: 500;
+	margin-top: 10px;
+`;
+
+const MoviesGrid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	grid-gap: 25px;
+	width: 80%;
+	position: relative;
+	top: -50px;
+	place-items: center;
+`;
+
+const PosterContainer = styled.div`
+	height: 450px;
+	border-radius: 10px;
+	width: 100%;
+	box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+	background-color: transparent;
+`;
+
+const PosterBg = styled.div`
+	background-image: url(${(props) => props.background});
+	height: 100%;
+	width: 100%;
+	background-size: cover;
+	background-position: center center;
+	border-radius: 7px;
+`;
 
 export default function Movies() {
-	const [movies, setMovies] = useState([]);
-	const client = useApolloClient();
-	useEffect(() => {
-		client
-			.query({
-				query: gql`
-					{
-						allMovies {
-							title
-							id
-						}
-					}
-				`,
-			})
-			.then((results) => setMovies(results.data.allMovies));
-	}, [client]);
+	const { data, loading } = useQuery(ALL_MOVIES);
 	return (
-		<ul>
-			{movies.map((movie) => (
-				<li key={movie.id}>{movie.title}</li>
-			))}
-		</ul>
+		<Container>
+			<Header>
+				<Title>Apollo Movies</Title>
+			</Header>
+			{loading && <Loading>Loading...</Loading>}
+			<MoviesGrid>
+				{data?.allMovies?.map((movie) => (
+					<PosterContainer key={movie.id}>
+						<Link to={`/movies/${movie.id}`}>
+							<PosterBg background={movie.medium_cover_image} />
+						</Link>
+					</PosterContainer>
+				))}
+			</MoviesGrid>
+		</Container>
 	);
 }
